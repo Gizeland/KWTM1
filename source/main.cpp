1460799c81cb8f0a1cc763718d3d45a377418ebb
Dominika Giza <domigiz518@student.polsl.pl>
2022-03-19 10:17:49 +0100

    Commit trzeci oraz czwarty

--- source/main.cpp
+++ source/main.cpp
@@ -2,8 +2,12 @@ int main()
 #include<trygonometria.h>
 #include<cmath>
 #include<iostream>
+#include<LaborkaConfig.h>
+
 int main()
 { /*...*/
+#ifdef USE_TRIGONOMETRY_DEGREE
+	std::cout << "USE_TRIGONOMETRY_DEGREE set as TRUE \n";
 	double sin = degreemath::sin(90);
 	std::cout << "Sin(90) = " << sin << "\n";
 	double cos = degreemath::cos(90);
@@ -12,5 +16,18 @@ int main()
 	std::cout << "Tan(180) = " << tan << "\n";
 	double ctg = degreemath::ctg(90);
 	std::cout << "Ctg(90) = " << ctg << "\n";
+
+#else
+	std::cout << "USE_TRIGONOMETRY_DEGREE set as FALSE \n";
+	double sinT = sin(90);
+	std::cout << "Sin(90) = " << sinT << "\n";
+	double cosT = cos(90);
+	std::cout << "Cos(90) = " << cosT << "\n";
+	double tanT = tan(180);
+	std::cout << "Tan(180) = " << tanT << "\n";
+	double ctgT = 1 / tan(90);
+	std::cout << "Ctg(90) = " << ctgT << "\n";
+#endif
+
 	return 0;
 }
